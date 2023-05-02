09336866c1be9f2b279b7f6e24b63896e1b1c7c0
MoonStyIe <mvjvl1793@naver.com>
2023-05-02 13:59:09 +0900

    file updated

--- app.py
+++ app.py
@@ -1,63 +1,90 @@
 # -*- coding:UTF-8 -*-
 import streamlit as st
-from utils import p_lans
-from utils import html_temp
-from utils import markdown_text
-from utils import markdown_info
+from PIL import Image
+import numpy as np
+import pandas as pd
 from eda_app import run_eda_app
 from ml_app import run_ml_app
+from streamlit_option_menu import option_menu
+
+# Confit
+st.set_page_config(page_title='Parkinson Disease Progression Prediction', page_icon=':medical_symbol:')
+# st.set_page_config(page_title='Parkinson Disease Progression Prediction', page_icon=':medical_symbol:', layout='wide')
+
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Introduce", 'Settings'],
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
 
 def main():
-    # st.markdown(html_temp, unsafe_allow_html=True)
+    menu = ['INTRODUCE', 'EDA', 'STAT', 'ML', 'About']
+    choice = st.sidebar.selectbox('Menu', menu)
 
-    menu = [
-        {"label": "Description"},
-        {"label": "EDA"},
-        {"label": "Machine Learning"},
-        {"label": "About"}
-    ]
+    if choice == 'INTRODUCE':
+        st.markdown(
+            "<h1 span style='text-align: center; color: darkblue;'>AMP®-Parkinson's </span><span style='text-align: center; color: darkmagenta;'>Disease Progression Prediction</span>",
+            unsafe_allow_html=True)
 
-    choice = st.sidebar.selectbox('Menu', [item['label'] for item in menu])
+        # Tool
+        c1, c2, c3, c4, c5 = st.columns(5)
+        c1.image(Image.open('data/excel.png'))
+        c2.image(Image.open('data/power point.png'))
+        c3.image(Image.open('data/kaggle.png'))
+        c4.image(Image.open('data/python.png'))
+        c5.image(Image.open('data/pycharm.png'))
 
-    # menu = ['Description', 'EDA', 'ML', 'About']
-    # choice = st.sidebar.selectbox('Menu',menu)
+        # Content
+        st.markdown("<h2 style='font-size: 24px; color: black;'>🔍 Goal of the Competition</h2>", unsafe_allow_html=True)
+        st.write(
+            """
+            The goal of this competition is to predict MDS-UPDR scores, which measure progression in patients with Parkinson's disease. 
+            The Movement Disorder Society-Sponsored Revision of the Unified Parkinson's Disease Rating Scale (MDS-UPDRS) is a comprehensive assessment of both motor and non-motor symptoms associated with Parkinson's. 
+            You will develop a model trained on data of protein and peptide levels over time in subjects with Parkinson’s disease versus normal age-matched control subjects. 
+            Your work could help provide important breakthrough information about which molecules change as Parkinson’s disease progresses.
+            """
+        )
 
-    if choice == 'Description':
-        st.markdown("<h1 span style='text-align: center; color: darkblue;'>AMP®-Parkinson's </span><span style='text-align: center; color: darkmagenta;'>Disease Progression Prediction</span><br></br>",
-                    unsafe_allow_html=True)
-        st.markdown("""
-            <style>
-                @font-face {
-                    font-family: 'JalnanOTF';
-                    src: url('./assets/JalnanOTF.otf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                }
-                h3, span {
-                    font-family: 'JalnanOTF';
-                }
-            </style>
-        """, unsafe_allow_html=True)
-        st.markdown("<h2 style='font-size: 24px; color: black;'>🔍 파킨슨병 대회개요</h2>", unsafe_allow_html=True)
-        # st.markdown("<h3><span style='font-size: 16px; color: black;'>👉 </span><span style='font-size: 16px; color: black;'>파킨슨병 환자의 단백질 및 펩타이드 데이터 측정을 사용하여 질병의 진행을 예측합니다.</span></h3>",
-        #             unsafe_allow_html=True)
-        show_info1 = st.checkbox('더보기')
-        if show_info1:
-            st.info(markdown_info)
-        st.markdown("<h4 style='font-size: 24px; color: black;'>🔬 파킨슨병 연구배경</h2>", unsafe_allow_html=True)
+        st.write('<hr>', unsafe_allow_html=True)
 
-        show_info = st.checkbox('더보기', key='my_checkbox')
-        if show_info:
-            st.info(markdown_text)
+        st.markdown("<h4 style='font-size: 24px; color: black;'>🔬 Context</h2>", unsafe_allow_html=True)
+        st.write(
+            """
+    Parkinson’s disease (PD) is a disabling brain disorder that affects movements, cognition, sleep, and other normal functions. 
+    Unfortunately, there is no current cure—and the disease worsens over time. It's estimated that by 2037, 1.6 million people in the U.S. 
+    will have Parkinson’s disease, at an economic cost approaching $80 billion. 
+    Research indicates that protein or peptide abnormalities play a key role in the onset and worsening of this disease. 
+    Gaining a better understanding of this—with the help of data science—could provide important clues for the development of new pharmacotherapies to slow the progression or cure Parkinson’s disease.
+    
+    Current efforts have resulted in complex clinical and neurobiological data on over 10,000 subjects for broad sharing with the research community. 
+    A number of important findings have been published using this data, but clear biomarkers or cures are still lacking.
+    
+    Competition host, the Accelerating Medicines Partnership® Parkinson’s Disease (AMP®PD), is a public-private partnership between government, industry, 
+    and nonprofits that is managed through the Foundation of the National Institutes of Health (FNIH). The Partnership created the AMP PD Knowledge Platform, 
+    which includes a deep molecular characterization and longitudinal clinical profiling of Parkinson’s disease patients, with the goal of identifying and validating diagnostic, 
+    prognostic, and/or disease progression biomarkers for Parkinson’s disease.
+    
+    Your work could help in the search for a cure for Parkinson’s disease, which would alleviate the substantial suffering and medical care costs of patients with this disease.
+            """
+        )
+
+        # sns
+        c1, c2, c3 = st.columns(3)
+        with c1:
+            st.info('**Data Analyst: [@Sung](https://muhanyuljung.tistory.com/)**', icon="💡")
+        with c2:
+            st.info('**GitHub: [@MST](https://github.com/MoonStyIe/Parkinson)**', icon="💻")
+        with c3:
+            st.info(
+                '**Data: [Kaggle](https://www.kaggle.com/competitions/amp-parkinsons-disease-progression-prediction)**',
+                icon="🧠")
 
     elif choice == 'EDA':
         run_eda_app()
-    elif choice == 'Machine Learning':
+    elif choice == 'ML':
         run_ml_app()
     else:
         st.subheader('About')
-
-
 
 if __name__ == "__main__":
     main()
--- eda_app.py
+++ eda_app.py
@@ -9,86 +9,127 @@ import numpy as np
 
 
 def run_eda_app():
-    st.subheader("탐색적 자료 분석")
+    # st.markdown("<h1 style='font-size: 24px; color: black;'>EDA (Exploratory Data Analysis)</h1>", unsafe_allow_html=True)
+    st.markdown(
+        "<h1 span style='text-align: center; color: darkblue;'>EDA </span><span style='text-align: center; color: darkmagenta;'>(Exploratory Data Analysis)</span>",
+        unsafe_allow_html=True)
+    # "<h1 span style='text-align: center; color: darkblue;'>EDA</span>",
+    # unsafe_allow_html = True)
 
-    iris = pd.read_csv('data/iris.csv')
-    st.markdown('## IRIS 데이터 확인')
-    st.write(iris) # print() 라고 생각
+    supplemental = pd.read_csv('data/supplemental_clinical_data.csv')
+    st.markdown("<h2 style='font-size: 24px; color: black;'>📋 Supplemental_clinical_data</h2>", unsafe_allow_html=True)
+    st.write(supplemental) # print() 라고 생각
+
+    st.markdown("<h3 style='font-size: 24px; color: black;'>🗄️ 데이터 타입</h3>", unsafe_allow_html=True)
+    result1 = pd.DataFrame(supplemental.dtypes)
+    st.write(result1)
+
+    st.write('<hr>', unsafe_allow_html=True)
+
+    train = pd.read_csv('data/train_clinical_data.csv')
+    st.markdown("<h4 style='font-size: 24px; color: black;'>📋 train_clinical_data</h4>", unsafe_allow_html=True)
+    st.write(train) # print() 라고 생각
+
+    st.markdown("<h5 style='font-size: 24px; color: black;'>🗄️ 데이터 타입</h5>", unsafe_allow_html=True)
+    result2 = pd.DataFrame(train.dtypes)
+    st.write(result2)
+
+    st.write('<hr>', unsafe_allow_html=True)
+
+    peptides = pd.read_csv('data/train_peptides.csv')
+    st.markdown("<h6 style='font-size: 24px; color: black;'>📋 train_peptides_data</h6>", unsafe_allow_html=True)
+    st.write(peptides) # print() 라고 생각
+
+    st.markdown("<h7 style='font-size: 24px; color: black;'>🗄️ 데이터 타입</h7>", unsafe_allow_html=True)
+    result3 = pd.DataFrame(peptides.dtypes)
+    st.write(result3)
+
+    st.write('<hr>', unsafe_allow_html=True)
+
+    proteins = pd.read_csv('data/train_proteins.csv')
+    st.markdown("<h8 style='font-size: 24px; color: black;'>📋 train_proteins_data</h8>", unsafe_allow_html=True)
+    st.write(proteins) # print() 라고 생각
+
+    st.markdown("<h9 style='font-size: 24px; color: black;'>🗄️ 데이터 타입</h9>", unsafe_allow_html=True)
+    result4 = pd.DataFrame(proteins.dtypes)
+    st.write(result4)
+
+    st.write('<hr>', unsafe_allow_html=True)
 
 
     # 메뉴 지정
     submenu = st.sidebar.selectbox('Submenu', ['기술통계량', '그래프분석', '통계분석'])
-    if submenu == '기술통계량':
-        st.dataframe(iris)
-
-        with st.expander("데이터 타입"):
-            result1 = pd.DataFrame(iris.dtypes)
-            st.write(result1)
-        with st.expander("기술 통계량"):
-            result2 = iris.describe()
-            st.write(result2)
-        with st.expander("타겟 빈도 수 확인"):
-            st.write(iris['species'].value_counts())
-    elif submenu == '그래프분석':
-        st.title("Title")
-        with st.expander("산점도"):
-            fig = px.scatter(iris, x = 'sepal_width',
-                             y = 'sepal_length',
-                             color = 'species',
-                             size = 'petal_width',
-                             hover_data=['petal_length'])
-            st.plotly_chart(fig)
-
-        # layouts
-        col1, col2 = st.columns(2)
-        with col1:
-            st.title('Seaborn')
-            # 그래프 작성
-            fig, ax = plt.subplots()
-            sns.boxplot(iris, x = 'petal_length', y = 'sepal_length', ax=ax)
-            st.pyplot(fig)
-
-        with col2:
-            st.title('Matplotlib')
-            # 그래프 작성
-            fig, ax = plt.subplots()
-            ax.hist(iris['sepal_length'], color='Darkgrey')
-            st.pyplot(fig)
-
-        # Tabs
-        tab1, tab2, tab3, tab4 = st.tabs(['탭1', '탭2', '탭3', '탭4'])
-        with tab1:
-            st.write('탭1')
-            # 종류 선택할 때마다
-            # 산점도 그래프가 달라지도록 한다.
-            # plotly 그래프로 구현
-            fig = px.scatter(iris, x='sepal_width',
-                             y='sepal_length',
-                             color='species',
-                             hover_data=['species'])
-            st.plotly_chart(fig)
-
-        with tab2:
-            st.write('탭2')
-            # 캐글 데이터 / 공모전 데이터
-            # 해당 데이터 그래프 1개만 그려본다.
-            clinical = pd.read_csv('data/supplemental_clinical_data.csv')
-            st.write(clinical)
-
-            fig, ax = plt.subplots()
-            ax.hist(clinical['updrs_1'], color='purple')
-            st.pyplot(fig)
-
-        with tab3:
-            st.write('탭3')
-
-        with tab4:
-            st.write('탭4')
-
-
-    elif submenu == '통계분석':
-        pass
-    else:
-        st.waring("WARING──!")
+    # if submenu == '기술통계량':
+    #     st.dataframe(supplemental)
+    #
+    #     with st.expander("데이터 타입"):
+    #         result1 = pd.DataFrame(supplemental.dtypes)
+    #         st.write(result1)
+    #     with st.expander("기술 통계량"):
+    #         result2 = supplemental.describe()
+    #         st.write(result2)
+    #     with st.expander("타겟 빈도 수 확인"):
+    #         st.write(supplemental['species'].value_counts())
+    # elif submenu == '그래프분석':
+    #     st.title("Title")
+    #     with st.expander("산점도"):
+    #         fig = px.scatter(supplemental, x = 'sepal_width',
+    #                          y = 'sepal_length',
+    #                          color = 'species',
+    #                          size = 'petal_width',
+    #                          hover_data=['petal_length'])
+    #         st.plotly_chart(fig)
+    #
+    #     # layouts
+    #     col1, col2 = st.columns(2)
+    #     with col1:
+    #         st.title('Seaborn')
+    #         # 그래프 작성
+    #         fig, ax = plt.subplots()
+    #         sns.boxplot(supplemental, x = 'petal_length', y = 'sepal_length', ax=ax)
+    #         st.pyplot(fig)
+    #
+    #     with col2:
+    #         st.title('Matplotlib')
+    #         # 그래프 작성
+    #         fig, ax = plt.subplots()
+    #         ax.hist(supplemental['sepal_length'], color='Darkgrey')
+    #         st.pyplot(fig)
+    #
+    #     # Tabs
+    #     tab1, tab2, tab3, tab4 = st.tabs(['탭1', '탭2', '탭3', '탭4'])
+    #     with tab1:
+    #         st.write('탭1')
+    #         # 종류 선택할 때마다
+    #         # 산점도 그래프가 달라지도록 한다.
+    #         # plotly 그래프로 구현
+    #         fig = px.scatter(supplemental, x='sepal_width',
+    #                          y='sepal_length',
+    #                          color='species',
+    #                          hover_data=['species'])
+    #         st.plotly_chart(fig)
+    #
+    #     with tab2:
+    #         st.write('탭2')
+    #         # 캐글 데이터 / 공모전 데이터
+    #         # 해당 데이터 그래프 1개만 그려본다.
+    #         clinical = pd.read_csv('data/supplemental_clinical_data.csv')
+    #         st.write(clinical)
+    #
+    #         fig, ax = plt.subplots()
+    #         ax.hist(clinical['updrs_1'], color='purple')
+    #         st.pyplot(fig)
+    #
+    #     with tab3:
+    #         st.write('탭3')
+    #
+    #     with tab4:
+    #         st.write('탭4')
+    #
+    #
+    # elif submenu == '통계분석':
+    #     pass
+    # else:
+    #     st.waring("WARING──!")
 
 
